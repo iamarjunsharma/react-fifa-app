07aa29a49d8ac089e58dd918002d5de18391aff0
iamarjunsharma <arjun.sharma17@gmail.com>
2017-10-31 01:40:54 +0530

    Added Form

--- app/components/App.js
+++ app/components/App.js
@@ -4,7 +4,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
-        Hello
+        <h1>Get Match Results of Your Favorite Football Team</h1>
+        <form>
+          <input type="text" className="input" placeholder="Please enter FIFA code of the country..." />
+          <input type="submit" className="submit-btn" value="Submit" />
+        </form>
       </div>
     );
   }
